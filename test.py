207a94f88c6e1355f8aa54d3d4dc7b46e066d915
shebin007 <shebinkp7@gmail.com>
2018-07-28 21:29:04 +0530

    Final Commit of a Python Program to print given list in Reverse Order

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-print "hello Gdg  "
-print "this for GDG"
-
-
+list = [12,24,35,24,24,88,120,155,88,120,155]
+range  = []
+for i in list[::-1]:
+  flag = 0
+  for j in range :
+    if i == j:
+      flag = 1
+  if flag == 1 :
+    continue
+  else :
+    range.append(i)
+	
+print range
